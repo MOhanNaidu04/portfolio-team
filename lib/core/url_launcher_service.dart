444cfb60d1469e9624e8f88d4a39728d75c84152
MOhanNaidu04 <chintanipplamohan549@email.com>
2026-07-16 14:15:28 +0530

    v10

--- lib/core/url_launcher_service.dart
+++ lib/core/url_launcher_service.dart
@@ -2,33 +2,32 @@ import 'package:url_launcher/url_launcher.dart';
 
 class UrlLauncherService {
   static Future<void> openUrl(String url) async {
-    final uri = Uri.parse(url);
+    final Uri uri = Uri.parse(url);
 
-    if (!await launchUrl(
-      uri,
-      mode: LaunchMode.externalApplication,
-    )) {
+    if (!await launchUrl(uri)) {
       throw Exception('Could not launch $url');
     }
   }
 
-  static Future<void> sendUrl(String url) async {
-  final Uri uri = Uri.parse(url);
+  static Future<void> sendEmail(String email) async {
+    final Uri uri = Uri(
+      scheme: 'mailto',
+      path: email,
+    );
 
-  if (!await launchUrl(
-    uri,
-    mode: LaunchMode.platformDefault,
-  )) {
-    throw Exception('Could not launch $url');
+    if (!await launchUrl(uri)) {
+      throw Exception('Could not launch email');
+    }
   }
-}
 
   static Future<void> makePhoneCall(String phone) async {
-    final uri = Uri(
+    final Uri uri = Uri(
       scheme: 'tel',
       path: phone,
     );
 
-    await launchUrl(uri);
+    if (!await launchUrl(uri)) {
+      throw Exception('Could not launch phone');
+    }
   }
 }
